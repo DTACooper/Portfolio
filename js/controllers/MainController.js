268f482c499c00b5f29bfe8eb82b373b1891ba79
Daniel Cooper <danielcooper@hotmail.co.uk>
2017-04-08 23:29:03 +0100

    fixed bug
    removed action which and fixed the name of the php file

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -148,7 +148,7 @@ app.controller('ContactController', function ($scope, $http) {
     $scope.result = 'hidden'
     $scope.resultMessage;
     $scope.formData; //formData is an object holding the name, email, subject, and message
-    $scope.submitButtonDisabled = true;
+    $scope.submitButtonDisabled = false;
     $scope.submitted = false; //used so that form errors are shown only after the form has been submitted
     $scope.submit = function(contactform) {
         $scope.submitted = true;
@@ -156,7 +156,7 @@ app.controller('ContactController', function ($scope, $http) {
         if (contactform.$valid) {
             $http({
                 method  : 'POST',
-                url     : '/php/formprocess.php',
+                url     : 'php/formprocess.php',
                 data    : $.param($scope.formData),  //param method from jQuery
                 headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  //set the headers so angular passing info as form data (not request payload)
             }).success(function(data){
