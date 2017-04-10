54272c6157cccc1cb4ed47a2e8c03dde3e950b75
Daniel Cooper <danielcooper@hotmail.co.uk>
2017-04-10 19:11:25 +0100

    Blog Post
    Playing around with a Blog Post AngularJS app, needs backend
    functionality

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -178,3 +178,45 @@ app.controller('ContactController', function ($scope, $http) {
         }
     }
 });
+
+app.controller('BlogController', ['$http', function($http){
+
+    var blog = this;
+    blog.title = "Blog posts";
+
+    blog.posts = {};
+    $http.get('/posts.json').success(function(data){
+      blog.posts = data;
+    });
+
+    blog.tab = 'blog';
+
+    blog.selectTab = function(setTab){
+      blog.tab = setTab;
+      console.log(blog.tab)
+    };
+
+    blog.isSelected = function(checkTab){
+      return blog.tab === checkTab;
+    };
+
+    blog.post = {};
+    blog.addPost = function(){
+      blog.post.createdOn = Date.now();
+      blog.post.comments = [];
+      blog.post.likes = 0;
+      blog.posts.unshift(this.post);
+      blog.tab = 0;
+      blog.post ={};
+    };
+
+  }]);
+
+  app.controller('CommentController', function(){
+    this.comment = {};
+    this.addComment = function(post){
+      this.comment.createdOn = Date.now();
+      post.comments.push(this.comment);
+      this.comment ={};
+    };
+  });
